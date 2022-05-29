b23ac13e845ecd0148c8f129d9da720d2b2117fc
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2022-05-29 11:34:05 +0200

    Fix license header

--- src/Result.php
+++ src/Result.php
@@ -1,7 +1,6 @@
 <?php
 
 /*
- * SPDX-FileCopyrightText: 2022 Daniele Corsini <daniele.corsini@corsinvest.it>
  * SPDX-FileCopyrightText: Copyright Corsinvest Srl
  * SPDX-License-Identifier: GPL-3.0-only
  */
